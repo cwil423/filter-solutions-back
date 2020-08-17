f68cae2403108163afb8543475b5c63960fda935
Cole Williamson <williamsoncole@gmail.com>
2020-08-17 16:02:06 -0500

    Removed sensitive information.

--- routes/mapquest.js
+++ routes/mapquest.js
@@ -10,8 +10,7 @@ router.post('/', (req, res) => {
   const customerAddresses = customers.map(element => element.address.Line1 + ' ' + element.address.City + ' ' + element.address.CountrySubDivisionCode)
   axios({
     method: 'post',
-    // url: `http://www.mapquestapi.com/directions/v2/optimizedroute?key=${process.env.mapquestKey}`,
-    url: 'http://www.mapquestapi.com/directions/v2/optimizedroute?key=rME5BfePWsNRXKfmq7ZZb3uT7RjwObvS',
+    url: `http://www.mapquestapi.com/directions/v2/optimizedroute?key=${process.env.mapquestKey}`,
     data: {
       "locations": [
         ...customerAddresses
